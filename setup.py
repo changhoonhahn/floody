45ec355aad905169b349d9d5443c16b87099adda
ChangHoon Hahn <attendee@hackweek.com>
2024-10-15 17:30:55 -0400

    install setup implemented

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+
+long_description = open('README.md').read()
+
+setup(
+    name="floody",
+    description="forecasting flood losses and savings",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    version="0.0.0",
+    license="MIT",
+    author="ChangHoon Hahn",
+    author_email="changhoon.hahn@princeton.edu",
+    url="https://github.com/changhoonhahn/floody",
+    packages=find_packages(where="src"), 
+    package_dir={"": "src"},
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python"
+    ],
+    keywords = ['machinelearning'],
+    install_requires=["torch", "numpy", "astropy", "sbi"]
+)
---
+++ src/floody/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+__all__ = [""]
+
+__author__ = "ChangHoon Hahn"
+__email__ = "changhoon.hahn@princeton.edu"
+__uri__ = "https://github.com/changhoonhahn/floody"
+__license__ = "MIT"
+__copyright__ = "Copyright 2024 ChangHoon Hahn"
